5711494fc97b284ac482a62020450a49d07a854a
SampavyKangeyan <sam990731@gamil.com>
2026-05-26 06:00:12 -0400

    Created total number of ratings

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -8,18 +8,21 @@ class ContentRating extends Component {
      this.state = {
       likes: 0,
       dislikes: 0,
+      totalRatings: 0,
       handleLike:() => {
         this.setState((prevState) => ({
-          likes: prevState.likes + 1
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings +1
+        }));
+      },
+      handleDislike:() => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings +1
         }));
       },
 
-      handleDislike:() => {
-        this.setState((prevState) => ({
-          dislikes: prevState.dislikes + 1
-        }));
     }
-  }
 }
   render() {
     return (
@@ -35,6 +38,11 @@ class ContentRating extends Component {
                 <button className="dislike-button" onClick={this.state.handleDislike}>
                     Dislike ({this.state.dislikes})
                 </button>
+            </div>
+            <div>
+                <p>
+                This shows totalRatings ({this.state.totalRatings})
+                </p>
             </div>
         </div>
      </>
